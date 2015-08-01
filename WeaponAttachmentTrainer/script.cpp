536c15f90b96826a04a22a22fb337d4eae54060d
zechs6437 <zechs6437@gmail.com>
2015-07-31 19:25:56 -0700

    Removed unused project code

--- WeaponAttachmentTrainer/script.cpp
+++ WeaponAttachmentTrainer/script.cpp
@@ -14,13 +14,9 @@ Hash gripHash = 0xC164F53;
 Hash flashlightHashes[2] = { 0x359B7AAE, 0x7BC4CDDC };
 Hash scopeHashes[7] = { 0x9D2FBF29, 0xA0D89C42, 0xAA2C45B4, 0xD2443DDC, 0x3CC6BA57, 0x3C00AFED, 0xBC54DA77 };
 
-bool featureNiceFly = GetPrivateProfileInt("Toggles", "NiceFly", 0, "./Ragdolla.ini");
-bool featureGTAIVCarExit = GetPrivateProfileInt("Toggles", "GTAIVCarExit", 0, "./Ragdolla.ini");
-bool featureRagdoll = GetPrivateProfileInt("Toggles", "Ragdoll", 0, "./Ragdolla.ini");
-bool featureAlwaysRadio = GetPrivateProfileInt("Toggles", "AlwaysRadio", 1, "./Ragdolla.ini");
-bool featureAlwaysRadioUpdated = false;
-bool featureCarUpgrade = GetPrivateProfileInt("Toggles", "CarUpgrade", 1, "./Ragdolla.ini");
-bool featureSilencerToggle = GetPrivateProfileInt("Toggles", "SilencerToggle", 1, "./Ragdolla.ini");
+bool featureSilencerToggle = GetPrivateProfileInt("Toggles", "SilencerToggle", 1, "./WeaponAttachmentTrainer.ini");
+int trainerToggle = GetPrivateProfileInt("Hotkeys", "WATHotkey", 0x74, "./WeaponAttachmentTrainer.ini");
+
 bool featureWeaponSilencer = false;
 bool featureWeaponMagazine = false;
 bool featureWeaponGrip = false;
@@ -64,7 +60,7 @@ void set_status_text(std::string str, DWORD time = 2500, bool isGxtEntry = false
 
 bool trainer_switch_pressed()
 {
-	return IsKeyJustUp(VK_F5);
+	return IsKeyJustUp(trainerToggle);
 }
 
 void get_button_state(bool *a, bool *b, bool *up, bool *down, bool *l, bool *r)
@@ -165,114 +161,6 @@ std::string line_as_str(std::string text, bool *pState)
 {
 	while (text.size() < 18) text += " ";
 	return text + (pState ? (*pState ? " [ON]" : " [OFF]") : "");
-}
-
-void gtaivCarExit()
-{
-	Ped playerPed = PLAYER::PLAYER_PED_ID();
-	//Player player = PLAYER::GET_PLAYER_PED(playerPed);
-	Any playerVehicle = PED::GET_VEHICLE_PED_IS_USING(playerPed);
-	//float playerVehicleSpeed = ENTITY::GET_ENTITY_SPEED(playerVehicle);
-	Any isPlayerDriving = (VEHICLE::GET_PED_IN_VEHICLE_SEAT(playerVehicle, -1) == playerPed);
-
-	if (isPlayerDriving)
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			VEHICLE::SET_VEHICLE_ENGINE_ON(playerVehicle, true, true);
-			WAIT(0);
-		}
-		WAIT(240);
-		if (CONTROLS::IS_CONTROL_PRESSED(2, 75))
-		{
-			VEHICLE::SET_VEHICLE_ENGINE_ON(playerVehicle, false, true);
-		}
-		else
-		{
-			VEHICLE::SET_VEHICLE_ENGINE_ON(playerVehicle, true, true);
-		}
-		//WAIT(1000);
-	}
-}
-
-void carModder()
-{
-	Ped playerPed = PLAYER::PLAYER_PED_ID();
-	Any player = PLAYER::GET_PLAYER_PED(playerPed);
-	BOOL playerExists = ENTITY::DOES_ENTITY_EXIST(playerPed);
-
-	if (playerExists) {
-		if (PED::IS_PED_IN_ANY_VEHICLE(playerPed, false)) {
-			Any veh = PED::GET_VEHICLE_PED_IS_IN(playerPed, true);
-			VEHICLE::SET_VEHICLE_FIXED(PED::GET_VEHICLE_PED_IS_USING(playerPed));
-			VEHICLE::SET_VEHICLE_DIRT_LEVEL(veh, 0);
-			VEHICLE::SET_VEHICLE_TYRES_CAN_BURST(veh, false);
-			VEHICLE::SET_VEHICLE_MOD_KIT(veh, 0);
-			//VEHICLE::SET_VEHICLE_COLOURS(veh, 120, 120); //color
-			VEHICLE::TOGGLE_VEHICLE_MOD(veh, 18, true); //turbo
-			VEHICLE::TOGGLE_VEHICLE_MOD(veh, 22, true); //xenon
-			VEHICLE::SET_VEHICLE_MOD(veh, 0, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 0) - 1, true); //spoiler
-			VEHICLE::SET_VEHICLE_MOD(veh, 1, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 1) - 1, true); //frontbumper
-			VEHICLE::SET_VEHICLE_MOD(veh, 2, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 2) - 1, true); //rearbumper
-			VEHICLE::SET_VEHICLE_MOD(veh, 3, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 3) - 1, true); //side skirt
-			VEHICLE::SET_VEHICLE_MOD(veh, 4, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 4) - 1, true); //exhaust
-			VEHICLE::SET_VEHICLE_MOD(veh, 5, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 5) - 1, true); //frame
-			VEHICLE::SET_VEHICLE_MOD(veh, 6, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 6) - 1, true); //grill
-			VEHICLE::SET_VEHICLE_MOD(veh, 7, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 7) - 1, true); //hood
-			VEHICLE::SET_VEHICLE_MOD(veh, 8, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 8) - 1, true); //fender
-			VEHICLE::SET_VEHICLE_MOD(veh, 9, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 9) - 1, true); //right fender
-			VEHICLE::SET_VEHICLE_MOD(veh, 10, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 10) - 1, true); //roof
-			VEHICLE::SET_VEHICLE_MOD(veh, 11, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 11) - 1, true); //engine
-			VEHICLE::SET_VEHICLE_MOD(veh, 12, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 12) - 1, true); //brakes
-			VEHICLE::SET_VEHICLE_MOD(veh, 13, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 13) - 1, true); //tranny
-			//VEHICLE::SET_VEHICLE_MOD(veh, 14, 14, true); //horn
-			VEHICLE::SET_VEHICLE_MOD(veh, 15, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 15) - 1, true); //suspension
-			VEHICLE::SET_VEHICLE_MOD(veh, 16, VEHICLE::GET_NUM_VEHICLE_MODS(veh, 16) - 1, true); //armor
-			//printf("Armor Mods:%d\n", VEHICLE::GET_NUM_VEHICLE_MODS(veh, 16));
-			//VEHICLE::SET_VEHICLE_MOD(veh, 23, 19, true); //front wheels
-			//VEHICLE::SET_VEHICLE_MOD_COLOR_1(veh, 3, 0, 0);
-			//VEHICLE::SET_VEHICLE_MOD_COLOR_2(veh, 3, 0);
-			VEHICLE::SET_VEHICLE_WHEEL_TYPE(veh, 6); //rims
-			VEHICLE::SET_VEHICLE_WINDOW_TINT(veh, 1);
-			set_status_text("Car Fully Upgraded");
-		}
-	}
-
-}
-
-void ragdoller()
-{
-	Ped playerPed = PLAYER::PLAYER_PED_ID();
-	BOOL playerExists = ENTITY::DOES_ENTITY_EXIST(playerPed);
-	if (!playerExists) return;
-	PED::SET_PED_CAN_RAGDOLL(playerPed, true);
-	PED::SET_PED_TO_RAGDOLL(playerPed, 0, 0, 0, false, false, false);
-}
-
-void niceFly()
-{
-	Player player = PLAYER::PLAYER_ID();
-	Ped playerPed = PLAYER::PLAYER_PED_ID();
-	//Any player = PLAYER::GET_PLAYER_PED(playerPed);
-	if (CONTROLS::IS_CONTROL_PRESSED(0, 21))
-	{
-		GAMEPLAY::SET_SUPER_JUMP_THIS_FRAME(player);
-		if (!WEAPON::HAS_PED_GOT_WEAPON(playerPed, 4222310262, 0))
-		{
-			WEAPON::GIVE_WEAPON_TO_PED(playerPed, 4222310262, 1, true, true);
-		}
-		if (ENTITY::GET_ENTITY_HEIGHT_ABOVE_GROUND(playerPed) > 1.5)
-		{
-			if (PED::GET_PED_PARACHUTE_STATE(playerPed) != 3)
-			{
-				ENTITY::APPLY_FORCE_TO_ENTITY(playerPed, 0, 0, 10000, 0, 0, 0, 0, 0, true, false, false, false, false);
-			}
-			if (CONTROLS::IS_CONTROL_PRESSED(0, 31))
-			{
-				ENTITY::APPLY_FORCE_TO_ENTITY(playerPed, 0, 0, 0, 10000, 0, 0, 0, 0, true, false, false, false, false);
-			}
-		}
-	}
 }
 
 	/*
@@ -530,38 +418,11 @@ void update_features()
 {
 	update_status_text();
 
-	Player player = PLAYER::PLAYER_ID();
-	Ped playerPed = PLAYER::PLAYER_PED_ID();
-	BOOL bPlayerExists = ENTITY::DOES_ENTITY_EXIST(playerPed);
-
-	if (featureNiceFly && CONTROLS::IS_CONTROL_PRESSED(0, 21))
-		niceFly();
-
-	if (featureGTAIVCarExit && CONTROLS::IS_CONTROL_PRESSED(2, 75))
-		gtaivCarExit();
-
-	if (featureRagdoll && IsKeyDown(0x55)) // Keyboard 'U'
-		ragdoller();
-
-	if (featureAlwaysRadioUpdated)
-	{
-		if (bPlayerExists && !featureAlwaysRadio)
-			AUDIO::SET_MOBILE_RADIO_ENABLED_DURING_GAMEPLAY(false);
-		featureAlwaysRadioUpdated = false;
-	}
-	if (featureAlwaysRadio)
-	{
-		if (bPlayerExists)
-			AUDIO::SET_MOBILE_RADIO_ENABLED_DURING_GAMEPLAY(true);
-	}
-
-	if (featureCarUpgrade && IsKeyJustUp(0x12))
-		carModder();
-
-	Hash playerWeapon = WEAPON::GET_SELECTED_PED_WEAPON(playerPed);
-
 	if (featureSilencerToggle && IsKeyJustUp(0x4F)) //keyboard 'O' (O as in your mom saying "oh my god ken gimme dat cack")
 	{
+		Ped playerPed = PLAYER::PLAYER_PED_ID();
+		Hash playerWeapon = WEAPON::GET_SELECTED_PED_WEAPON(playerPed);
+
 		for (int a = 0; a < 5; a = a + 1)
 		{
 			if (WEAPON::_CAN_WEAPON_HAVE_COMPONENT(playerWeapon, silencerHashes[a]))
@@ -588,7 +449,7 @@ void process_weaponupgrade_menu()
 	const float lineWidth = 250.0;
 	const int lineCount = 6;
 
-	std::string caption = "Weapon Upgrades";
+	std::string caption = "Weapon Attachment Trainer by Gumshoe";
 
 	static struct {
 		LPCSTR		text;
@@ -635,9 +496,9 @@ void process_weaponupgrade_menu()
 			case 1:
 			{
 				if (!featureWeaponSilencer)
-					WritePrivateProfileString("Toggles", "SilencerToggle", " 1", "./Ragdolla.ini");
+					WritePrivateProfileString("Toggles", "SilencerToggle", " 1", "./WeaponAttachmentTrainer.ini");
 				else
-					WritePrivateProfileString("Toggles", "SilencerToggle", " 0", "./Ragdolla.ini");
+					WritePrivateProfileString("Toggles", "SilencerToggle", " 0", "./WeaponAttachmentTrainer.ini");
 				toggleSilencer();
 				featureWeaponSilencer = !featureWeaponSilencer;
 				break;
@@ -703,125 +564,6 @@ void process_weaponupgrade_menu()
 
 int activeLineIndexMain = 0;
 
-void process_main_menu()
-{
-	const float lineWidth = 250.0;
-	const int lineCount = 6;
-
-	std::string caption = "Maarek's Awesome Shit";
-
-	static struct {
-		LPCSTR		text;
-		bool		*pState;
-		bool		*pUpdated;
-	} lines[lineCount] = {
-		{ "Nice Fly", &featureNiceFly, NULL },
-		{ "GTAIV Car Exit", &featureGTAIVCarExit, NULL },
-		{ "Ragdoll (U)", &featureRagdoll, NULL },
-		{ "Always Radio", &featureAlwaysRadio, &featureAlwaysRadioUpdated },
-		{ "Car Upgrade (ALT)", &featureCarUpgrade, NULL },
-		{ "Weapon Upgrades", NULL, NULL }
-	};
-
-	DWORD waitTime = 150;
-	while (true)
-	{
-		// timed menu draw, used for pause after active line switch
-		DWORD maxTickCount = GetTickCount() + waitTime;
-		do
-		{
-			// draw menu
-			draw_menu_line(caption, lineWidth, 15.0, 18.0, 0.0, 5.0, false, true);
-			for (int i = 0; i < lineCount; i++)
-				if (i != activeLineIndexMain)
-					draw_menu_line(line_as_str(lines[i].text, lines[i].pState),
-					lineWidth, 9.0, 60.0 + i * 36.0, 0.0, 9.0, false, false);
-			draw_menu_line(line_as_str(lines[activeLineIndexMain].text, lines[activeLineIndexMain].pState),
-				lineWidth + 1.0, 11.0, 56.0 + activeLineIndexMain * 36.0, 0.0, 7.0, true, false);
-
-			update_features();
-			WAIT(0);
-		} while (GetTickCount() < maxTickCount);
-		waitTime = 0;
-
-		// process buttons
-		bool bSelect, bBack, bUp, bDown;
-		get_button_state(&bSelect, &bBack, &bUp, &bDown, NULL, NULL);
-		if (bSelect)
-		{
-			menu_beep();
-			switch (activeLineIndexMain)
-			{
-			case 0:
-				if (!featureNiceFly)
-					WritePrivateProfileString("Toggles", "NiceFly", " 1", "./Ragdolla.ini");
-				else
-					WritePrivateProfileString("Toggles", "NiceFly", " 0", "./Ragdolla.ini");
-				goto GHETTOJUMP;
-			case 1:
-				if (!featureGTAIVCarExit)
-					WritePrivateProfileString("Toggles", "GTAIVCarExit", " 1", "./Ragdolla.ini");
-				else
-					WritePrivateProfileString("Toggles", "GTAIVCarExit", " 0", "./Ragdolla.ini");
-				goto GHETTOJUMP;
-			case 2:
-				if (!featureRagdoll)
-					WritePrivateProfileString("Toggles", "Ragdoll", " 1", "./Ragdolla.ini");
-				else
-					WritePrivateProfileString("Toggles", "Ragdoll", " 0", "./Ragdolla.ini");
-				goto GHETTOJUMP;
-			case 3:
-				if (!featureAlwaysRadio)
-					WritePrivateProfileString("Toggles", "AlwaysRadio", " 1", "./Ragdolla.ini");
-				else
-					WritePrivateProfileString("Toggles", "AlwaysRadio", " 0", "./Ragdolla.ini");
-				goto GHETTOJUMP;
-			case 4:
-				if (!featureCarUpgrade)
-					WritePrivateProfileString("Toggles", "CarUpgrade", " 1", "./Ragdolla.ini");
-				else
-					WritePrivateProfileString("Toggles", "CarUpgrade", " 0", "./Ragdolla.ini");
-				goto GHETTOJUMP;
-			case 5:
-				weaponSanityCheck();
-				process_weaponupgrade_menu();
-				break;
-GHETTOJUMP: //this is so bad
-			default:
-				if (lines[activeLineIndexMain].pState)
-					*lines[activeLineIndexMain].pState = !(*lines[activeLineIndexMain].pState);
-				if (lines[activeLineIndexMain].pUpdated)
-					*lines[activeLineIndexMain].pUpdated = true;
-			}
-			waitTime = 200;
-		}
-		else
-		if (bBack || trainer_switch_pressed())
-		{
-			menu_beep();
-			break;
-		}
-		else
-		if (bUp)
-		{
-			menu_beep();
-			if (activeLineIndexMain == 0)
-				activeLineIndexMain = lineCount;
-			activeLineIndexMain--;
-			waitTime = 150;
-		}
-		else
-		if (bDown)
-		{
-			menu_beep();
-			activeLineIndexMain++;
-			if (activeLineIndexMain == lineCount)
-				activeLineIndexMain = 0;
-			waitTime = 150;
-		}
-	}
-}
-
 void main()
 {
 	while (true)
@@ -829,7 +571,7 @@ void main()
 		if (trainer_switch_pressed())
 		{
 			menu_beep();
-			process_main_menu();
+			process_weaponupgrade_menu();
 		}
 		update_features();
 		WAIT(0);
@@ -838,10 +580,6 @@ void main()
 
 void ScriptMain()
 {
-//	AllocConsole();
-//	freopen("CONIN$", "r", stdin);
-//	freopen("CONOUT$", "w", stdout);
-//	freopen("CONOUT$", "w", stderr);
 	srand(GetTickCount());
 	main();
 }
